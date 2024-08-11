03bee24f51b636aa1fa883743c0a6d2e9cfb5691
Aayushi Singh <aayushisingh@Aayushis-MacBook-Air.local>
2024-08-11 18:38:06 +0530

    added html tickets

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let addbtn = document.querySelector(".add-btn");
 let modal = document.querySelector(".modal-cont");
 let allPriorityColor = document.querySelectorAll(".priority-color");
+let textArea = document.querySelector(".textarea-cont");
+let mainCont = document.querySelector(".main-cont");
 
 let addModal = true;
 
@@ -23,4 +25,25 @@ for (let i = 0; i < allPriorityColor.length; i++) {
     }
     allPriorityColor[i].classList.add("active");
   });
+}
+
+textArea.addEventListener("keydown", function (e) {
+  // console.log(e);
+  let key = e.key;
+  if (key == "Enter") {
+    createTicket();
+    textArea.value = "";
+    modal.style.display = "none";
+    addModal = true;
+  }
+});
+
+function createTicket() {
+  let ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
+  ticketCont.innerHTML = `<div class="ticket-color red"></div>
+                        <div class="ticket-id">#gcvdvcjkj</div>
+                        <div class="ticket-area">Detail of your task</div>`;
+  console.log(ticketCont);
+  mainCont.append(ticketCont);
 }
